77b42b84c32beca5d5f3c965426d8d0c953e17c9
runnercam66 <rsantamariacastel@gmail.com>
2026-06-29 19:56:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,3 +4,6 @@ const FAMILIA_JSON_URL = 'https://raw.githubusercontent.com/runnercam66/arbre-ge
 const FORMSPREE_ID = 'meebzvag';
 const ADMIN_EMAIL = 'r.santamaria@haus-hb.com';
 const ARBRE_URL = 'https://runnercam66.github.io/arbre-genealogic';
+const FAMILIA_JSON_URL = 'https://raw.githubusercontent.com/runnercam66/arbre-genealogic/main/familia.json'
+const FORMSPREE_ID = 'meebzvag'
+const ARBRE_URL = 'https://runnercam66.github.io/arbre-genealogic'
